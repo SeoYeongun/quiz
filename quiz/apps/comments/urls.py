0480b6f3c17db5933c8a159f9ce803cbf6d1246d
SeoYeongun <voe697754@gmail.com>
2026-06-05 18:10:11 +0900

    comments2

--- quiz/apps/comments/urls.py
+++ quiz/apps/comments/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
-from rest_framework import routers
+from ntpath import basename
+from rest_framework.routers import DefaultRouter
 from .views import CommentViewSet
 
-router = routers.DefaultRouter()
-router.register(r'', CommentViewSet)
+router = DefaultRouter()
+router.register('comments', CommentViewSet, basename='comments')
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
--- quiz/apps/comments/views.py
+++ quiz/apps/comments/views.py
@@ -14,3 +14,18 @@ class CommentViewSet(viewsets.ModelViewSet):
         if quiz_id:
             return Comment.objects.filter(quiz_id=quiz_id)
         return super().get_queryset()
+    
+    def perform_create(self, serializer):
+        # Set the author to the current user and quiz to the specified quiz
+        quiz_id = self.request.data.get('quiz')
+        serializer.save(author=self.request.user, quiz_id=quiz_id)
+
+    def perform_update(self, serializer):
+        # Ensure that the author cannot be changed on update
+        serializer.save(author=self.get_object().author)
+
+    def perform_destroy(self, instance):
+        # Ensure that only the author can delete their comment
+        if instance.author != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this comment.")
+        instance.delete()
